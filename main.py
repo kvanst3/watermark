d4b22d9b3cbe50f18919a44e0808083155eb1f64
kvanst <kvanst3@gmail.com>
2021-07-19 15:20:34 +0200

    show a watermarked version of the original image

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import tkinter
 from tkinter import filedialog
 import os.path
-from PIL import ImageTk, Image  
+from PIL import ImageTk, Image, ImageDraw, ImageFont
+
+IMG = None
 
 window = tkinter.Tk()
 window.title("Watermarker")
@@ -11,15 +13,14 @@ window.config(padx=10, pady=10)
 
 # Title Label
 
-label = tkinter.Label(text="Upload image", font=("Arial"))
-label.grid(column=1, row=0)
+label_img_placeholder = tkinter.Label(text="Upload image", font=("Arial"))
+label_img_placeholder.grid(column=1, row=0)
 
 
 # file explorer window
 
 def browseFiles():
     file_path = filedialog.askopenfilename(
-        # mode='rb',
         initialdir=os.path.expanduser('~/Pictures'),
         title="Select a File",
         filetypes=(
@@ -27,20 +28,51 @@ def browseFiles():
             ("all files", "*.*")
         )
     )
-    img = Image.open(file_path)
-    display_image = ImageTk.PhotoImage(img)
-    label.config(image=display_image)
-    label.grid(columnspan=2)
-    label.image = display_image
+    global IMG
+    IMG = Image.open(file_path).convert("RGBA")
+    display_image = ImageTk.PhotoImage(IMG)
+    label_img_placeholder.config(image=display_image)
+
+    # move stuff around, change labels name, etc
+
+    label_img_placeholder.grid(columnspan=2)
+    label_img_placeholder.image = display_image
     button.grid(column=1, row=2)
     label_file_explorer.grid(column=2, row=2, sticky='E')
-    label_file_explorer.config(text=os.path.basename(file))
+    label_file_explorer.config(text=os.path.basename(file_path))
+
+    wtmrk_button.grid(column=1, row=3, sticky='W')
 
 
-#Browse Button
+def apply_watermark():
+    width, height = IMG.size
+
+    draw = ImageDraw.Draw(IMG)
+    text = "sample watermark"
+
+    font = ImageFont.truetype('/usr/share/fonts/opentype/malayalam/Chilanka-Regular.otf', 26)
+    textwidth, textheight = draw.textsize(text, font)
+
+    # calculate the x,y coordinates of the text
+    margin = 10
+    x = width - textwidth - margin
+    y = height - textheight - margin
+
+    # draw watermark in the bottom right corner
+    draw.text((x, y), text, font=font, fill=(255,255,255,150))
+    IMG.show()
+    
+
+
+# Browse Button
 
 button = tkinter.Button(text="Browse", command=browseFiles)
 button.grid(column=2, row=2, sticky='W')
+
+
+# Watermark Button
+
+wtmrk_button = tkinter.Button(text="Watermark it!", command=apply_watermark)
 
 
 # File explorer Label
